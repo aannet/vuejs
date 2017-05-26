c4918a3e30ec05a869e6e5c64eb9566dcd006899
alec.annet-desdame <alec.annet-desdame@alterway.fr>
2017-05-26 15:48:51 +0200

    DEV: new state Completed for taskes and RAZ button

--- web/mytodoapp.js
+++ web/mytodoapp.js
@@ -4,20 +4,29 @@ var apptodo = new Vue({
   data: {
     inputTask: '',
     todos: [
-      { text: 'Task 1-already filled' },
-      { text: 'Task 2-already filled' },
-      { text: 'Task 3-already filled' }
+      { title: 'Task 1-already filled', completed: false },
+      { title: 'Task 2-already filled', completed: true },
+      { title: 'Task 3-already filled', completed: false }
     ]
   },
   methods: {
-      submitInputTask: function() {
-          if (this.inputTask.trim() == '') {
-              alert('No empty task !!');
-          }
-          else {
-              this.todos.push({text: this.inputTask.trim()});
-          }
-          //var newtask = input-task;
+    submitInputTask: function() {
+      if (this.inputTask.trim() == '') {
+          alert('No empty task !!');
       }
+      else {
+          this.todos.push({title: this.inputTask.trim()});
+          this.inputTask = '';
+      }
+    },
+    submitRAZ: function() {
+        while(this.todos.length > 0) {
+            this.todos.pop();
+        }
+    },
+    setTaskDone: function (index) {
+        this.todos[index].completed = true;
+        //TODO: refreshing problem after changing state of newly added task
+    }
   }
 })
